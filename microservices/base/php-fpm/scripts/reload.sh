27dd7e37fae8478d6b69c1987254a5dcb2921f4f
Moustakime KIFIA <mkifia@icloud.com>
2019-08-13 12:29:24 +0200

    fix Dockerfile

--- microservices/base/php-fpm/scripts/reload.sh
+++ microservices/base/php-fpm/scripts/reload.sh
@@ -1,41 +1,41 @@
-    #!/bin/bash
+#!/bin/bash
 
-    SERVICE_NAME=${SERVICE_NAME:-nginx}
-    CONSUL=${CONSUL:-consul}
+SERVICE_NAME=${SERVICE_NAME:-nginx}
+CONSUL=${CONSUL:-consul}
 
-    preStart() {
-      consul-template \
-            -once \
-            -dedup \
-            -consul ${CONSUL}:8500 \
-            -template "/etc/nginx/nginx.conf.ctmpl:/etc/nginx/nginx.conf"
-    }
+preStart() {
+  consul-template \
+        -once \
+        -dedup \
+        -consul ${CONSUL}:8500 \
+        -template "/etc/nginx/nginx.conf.ctmpl:/etc/nginx/nginx.conf"
+}
 
-    onChange() {
-      consul-template \
-            -once \
-            -dedup \
-            -consul ${CONSUL}:8500 \
-            -template "/etc/nginx/nginx.conf.ctmpl:/etc/nginx/
-            nginx.conf:nginx -s reload"
-    }
+onChange() {
+  consul-template \
+        -once \
+        -dedup \
+        -consul ${CONSUL}:8500 \
+        -template "/etc/nginx/nginx.conf.ctmpl:/etc/nginx/
+        nginx.conf:nginx -s reload"
+}
 
-    help() {
-      echo "Usage: ./reload.sh preStart
-      => first-run configuration for Nginx"
-      echo "      ./reload.sh onChange  => [default] update Nginx config on
-      upstream changes"
-    }
+help() {
+  echo "Usage: ./reload.sh preStart
+  => first-run configuration for Nginx"
+  echo "      ./reload.sh onChange  => [default] update Nginx config on
+  upstream changes"
+}
 
-    until
-      cmd=$1
-      if [ -z "$cmd" ]; then
-             onChange
-      fi
-      shift 1
-      $cmd "$@"
-      [ "$?" -ne 127 ]
-    do
-       onChange
-       exit
-    done
+until
+  cmd=$1
+  if [ -z "$cmd" ]; then
+         onChange
+  fi
+  shift 1
+  $cmd "$@"
+  [ "$?" -ne 127 ]
+do
+   onChange
+   exit
+done
